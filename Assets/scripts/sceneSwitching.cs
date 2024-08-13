ef6ead3dfd8e569ff9360481386c72d55ec9e86c
TomTheTrex <142213215+TomTheTrex@users.noreply.github.com>
2024-08-13 14:14:38 +0800

    map 1

---
+++ Assets/scripts/sceneSwitching.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class sceneSwitching : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Task 2c - Load the scene using SceneManager.LoadScene()
+            SceneManager.LoadScene("level0");
+        }
+    }
+}
