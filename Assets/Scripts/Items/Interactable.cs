cd1acd56bc9163d1d20539df2c4a22ad80d66482
SyedIrfan74 <142783038+SyedIrfan74@users.noreply.github.com>
2024-08-14 12:20:28 +0800

    updated interactable

--- Assets/Scripts/Items/Interactable.cs
+++ Assets/Scripts/Items/Interactable.cs
@@ -2,7 +2,29 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
-    public float radius = 1f;
+    public float radius = 0.5f;
+
+    public CircleCollider2D circleCollider;
+
+    public Transform player;
+
+    public virtual void Interact()
+    {
+        //Meant to be Overwritten
+        Debug.Log("Interacting with " + transform.name);
+    }
+
+    private void Update()
+    {
+        circleCollider.radius = radius;
+
+        Vector3 distance = transform.position - player.position;
+
+        //if ()
+        //{
+
+        //}
+    }
 
     void OnDrawGizmosSelected()
     {
