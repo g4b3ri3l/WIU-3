7f030fecb995ff929a4fc02b90fa40cb8aa797bd
jothedelta <jorell.goh@gmail.com>
2024-08-15 14:45:56 +0800

    totalling master

--- Assets/UI/Inventory/InventoryUI.cs
+++ Assets/UI/Inventory/InventoryUI.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class InventoryUI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Inventory inventory;
     void Start()
     {
-        
+        inventory = Inventory.instance;
+        inventory.onItemChangedCallBack += UpdateUI;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void UpdateUI()
+    {
+
     }
 }
