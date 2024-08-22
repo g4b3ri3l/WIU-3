7a79f08e9207412756593c3b239924e002ef990b
TomTheTrex <142213215+TomTheTrex@users.noreply.github.com>
2024-08-22 13:30:16 +0800

    hmm

--- Assets/Scripts/UI/ConsumeGroup.cs
+++ Assets/Scripts/UI/ConsumeGroup.cs
@@ -6,11 +6,28 @@ public class ConsumeGroup : MonoBehaviour
 {
     [SerializeField] Item slot1, slot2;
     [SerializeField] PlayerManager manager;
+
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip EatClip;
+    [SerializeField] private float EatingCooldown = 1.0f;
+    private float EatingSoundCooldownTimer = 0f;
+
     void Update()
     {
+        if (EatingSoundCooldownTimer > 0)
+        {
+            EatingSoundCooldownTimer -= Time.deltaTime;
+        }
+
         if (Input.GetKeyDown(KeyCode.Alpha1)) {
             if (slot1.itemCount > 0 && manager.health != manager.maxHp)
             {
+
+                if (EatingSoundCooldownTimer <= 0f)
+                {
+                    audioSource.PlayOneShot(EatClip);
+                    EatingSoundCooldownTimer = EatingCooldown; // Reset the cooldown timer
+                }
                 slot1.itemCount--;
                 manager.Heal(10);
             }
@@ -19,6 +36,12 @@ public class ConsumeGroup : MonoBehaviour
         {
             if (slot2.itemCount > 0 && manager.health != manager.maxHp)
             {
+
+                if (EatingSoundCooldownTimer <= 0f)
+                {
+                    audioSource.PlayOneShot(EatClip);
+                    EatingSoundCooldownTimer = EatingCooldown; // Reset the cooldown timer
+                }
                 slot2.itemCount--;
                 manager.Heal(20);
             }
